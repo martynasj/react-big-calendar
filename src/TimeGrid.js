a020d56f7054b578586047a2ea2d4c89f01daae2
martynas <lyrinisobjektas@gmail.com>
2017-08-22 22:09:42 +0300

    fix/Display multiday events in week/day views

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -23,6 +23,8 @@ import { accessor as get } from './utils/accessors';
 
 import { inRange, sortEvents, segStyle } from './utils/eventLevels';
 
+window.dates = dates;
+
 export default class TimeGrid extends Component {
 
   static propTypes = {
@@ -167,6 +169,7 @@ export default class TimeGrid extends Component {
           || (dates.isJustDate(eStart) && dates.isJustDate(eEnd)))
         {
           allDayEvents.push(event)
+          rangeEvents.push(event)
         }
         else
           rangeEvents.push(event)
